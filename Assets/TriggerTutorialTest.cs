00407ae5da80a80ad9aa1e6586056083d828a82c
Anil Punjabi <anilo@hotmail.com>
2015-05-04 02:02:34 -0700

    testColliders

---
+++ Assets/TriggerTutorialTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerTutorialTest : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collisionInfo)
+	{
+		print("Detected trigger between " + gameObject.name + " and " + collisionInfo.GetComponent<Collider>().name);
+	}
+	
+	void OnTriggerStay(Collider collisionInfo)
+	{
+		print(gameObject.name + " and " + collisionInfo.GetComponent<Collider>().name + " are still colliding");
+	}
+	
+	void OnTriggerExit(Collider collisionInfo)
+	{
+		print(gameObject.name + " and " + collisionInfo.GetComponent<Collider>().name + " are no longer colliding");
+	}
+
+}
